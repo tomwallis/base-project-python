251f1156a27e901d59fe90dd53e8ad4be48d2b6a
tomwallis <tsawallis@gmail.com>
2018-11-07 22:01:44 +0100

    fixed run command.

--- example_run_command.sh
+++ example_run_command.sh
@@ -2,4 +2,4 @@
 
 docker run -p 8888:8888 -it \
 --mount type=bind,source="$(pwd)",destination=/home/jovyan/work \
-base-project-python start.sh jupyter lab
+tsawallis/base-project-python start.sh jupyter lab
